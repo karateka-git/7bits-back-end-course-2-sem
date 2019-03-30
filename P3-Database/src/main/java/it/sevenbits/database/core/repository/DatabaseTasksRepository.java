3fbb7fb8b265e9e5ba571a981cda28af84544b22
Vladislav <zvvlad2@gmail.com>
2019-03-30 17:36:23 +0600

    add column date

--- P3-Database/src/main/java/it/sevenbits/database/core/repository/DatabaseTasksRepository.java
+++ P3-Database/src/main/java/it/sevenbits/database/core/repository/DatabaseTasksRepository.java
@@ -7,7 +7,9 @@ import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class DatabaseTasksRepository implements TaskRepository {
@@ -23,25 +25,21 @@ public class DatabaseTasksRepository implements TaskRepository {
 
 
     private String getCurrentDate() {
-        java.util.Date utilDate = new java.util.Date();
-        java.sql.Timestamp sq = new java.sql.Timestamp(utilDate.getTime());
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
-        return sdf.format(sq);
+        Date utilDate = new Date();
+        Timestamp sq = new Timestamp(utilDate.getTime());
+        return sq.toString();
     }
 
     @Override
     public List<Task> getAllItems() {
         return jdbcOperations.query(
                 "SELECT id, text, status, createdAT FROM task",
-                new RowMapper<Task>() {
-                    public Task mapRow(ResultSet resultSet, int i) throws SQLException {
-                        long id = resultSet.getLong(1);
-                        String text = resultSet.getString(2);
-                        String status = resultSet.getString(3);
-                        String date = resultSet.getString(4);
-                        return new Task(id, text, status, date);
-                    }
+                (resultSet, i) -> {
+                    long id = resultSet.getLong(1);
+                    String text = resultSet.getString(2);
+                    String status = resultSet.getString(3);
+                    String date = resultSet.getString(4);
+                    return new Task(id, text, status, date);
                 });
     }
 
@@ -55,10 +53,11 @@ public class DatabaseTasksRepository implements TaskRepository {
         long id = getNextId();
         String status = EnumValues.EnumStatus.inbox.toString();
         String date = getCurrentDate();
+        System.out.print("\n\n\n\n\n\n" + date + "\n\n\n\n\n\n");
         Task task = new Task(id, text, status, date);
         int rows = jdbcOperations.update(
                 "INSERT INTO task (id, text, status, createdAT) VALUES (?, ?, ?, ?)",
-                id, text, status, date
+                id, text, status, Timestamp.valueOf(date)
         );
         return task;  // or select from DB
     }
