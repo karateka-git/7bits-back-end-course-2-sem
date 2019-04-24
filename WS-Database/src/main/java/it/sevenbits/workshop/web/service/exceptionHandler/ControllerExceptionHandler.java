6a9013ae9e552a1f11bd1c6cdc7896f2a3922682
Vladislav <zvvlad2@gmail.com>
2019-04-25 00:14:29 +0600

    readme.md formatting added

--- WS-Database/src/main/java/it/sevenbits/workshop/web/service/exceptionHandler/ControllerExceptionHandler.java
+++ WS-Database/src/main/java/it/sevenbits/workshop/web/service/exceptionHandler/ControllerExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * Exception handler for controller
+ */
 @ControllerAdvice
 public class ControllerExceptionHandler {
     /**
